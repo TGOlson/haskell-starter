d7482e084914423f06e9ca9410726cd9b89f002a
Tyler Olson <tydotg@gmail.com>
2018-03-16 17:29:32 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# haskell-starter
+
+```
+$ stack build
+$ stack test
+$ stack exec -- haskell-starter
+```
---
+++ lib/Data/Quicksort.hs
@@ -0,0 +1,6 @@
+module Data.Quicksort
+  ( quicksort
+  ) where
+
+quicksort :: Ord a => [a] -> [a]
+quicksort = undefined
